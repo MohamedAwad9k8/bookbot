4c9a0cc668d4f8fb1d6825cdbf38d7ed5abef6d9
M.Awad <75997594+MohamedAwad9k8@users.noreply.github.com>
2025-12-01 13:59:02 +0200

    reading books content and counting words

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("greetings boots")
+def main():
+    book_path = "./books/frankenstein.txt"
+    book_content = get_book_text(book_path)
+    num_words = get_word_count(book_content)
+    print(f"Found {num_words} total words")
+
+def get_book_text(file_path):
+    with open(file_path) as f:
+        file_contents = f.read()
+    return file_contents
+
+def get_word_count(content):
+    words_list = content.split()
+    return len(words_list)
+
+if __name__ == "__main__":
+    main()
